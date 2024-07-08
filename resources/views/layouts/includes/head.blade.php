2a7f03e1b9e8ececd7eb153d2826971158da7038
Murad <agamedov94@mail.ru>
2024-07-08 23:11:18 +0400

    start

---
+++ resources/views/layouts/includes/head.blade.php
@@ -0,0 +1,18 @@
+<head>
+
+    <meta charset="utf-8">
+    <title>Analytics | Tailwick - Admin & Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta content="Minimal Admin & Dashboard Template" name="description">
+    <meta content="Themesdesign" name="author">
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{asset('panel/assets/images/favicon.ico')}}">
+    <!-- Layout config Js -->
+    <script src="{{asset('panel/assets/js/layout.js')}}"></script>
+    <!-- Icons CSS -->
+
+    <!-- Tailwind CSS -->
+
+
+    <link rel="stylesheet" href="{{asset('panel/assets/css/tailwind2.css')}}">
+</head>
