191c6213e3816198d2b6e608f843084b0c7f559c
Murad Agamedov <agamedov94@mail.ru>
2024-07-18 09:15:29 +0400

    commit

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -65,7 +65,7 @@
 
         <div class="mb-0 w-screen lg:mx-auto lg:w-[500px] card shadow-lg border-none shadow-slate-100 relative">
             <div class="!px-10 !py-12 card-body">
-                <a href="#!">
+                <a>
                     <img src="{{asset('storage/')}}/{{ServiceFacade::getData()->with('images')->first()->images->where('type', 'nav_logo')->first()->url}}" alt="" class="hidden h-6 mx-auto dark:block">
                     <img src="{{asset('storage/')}}/{{ServiceFacade::getData()->with('images')->first()->images->where('type', 'nav_logo')->first()->url}}" alt="" class="block h-6 mx-auto dark:hidden">
                 </a>
